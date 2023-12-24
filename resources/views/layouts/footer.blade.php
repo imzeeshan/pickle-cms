7b77fa2b7a400e3d8a12e0a63be1e6898853511d
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2023-12-25 03:25:30 +0530

    December 2023 Updates
    December 2023 Updates

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,7 @@
 <footer class="footer footer-transparent d-print-none">
-    <div class="container-xl">
-      <div class="row text-center align-items-center flex-row-reverse">
-        <div class="col-lg-auto ms-lg-auto">
-          <ul class="list-inline list-inline-dots mb-0">
-            <li class="list-inline-item">
+    <div class="container text-center">    
               Copyright &copy; {{\Carbon\Carbon::now()->format('Y')}}
               <a href="." class="link-secondary">{{ env('APP_NAME') }}</a>.
               All rights reserved.
-            </li>           
-          </ul>
-        </div>
-      </div>
     </div>
-  </footer>
+</footer>
