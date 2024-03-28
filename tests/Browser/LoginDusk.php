e02d1c23076ab46bdf514f3a65d51205a3583135
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2024-03-29 01:53:04 +0530

    DUSK

---
+++ tests/Browser/LoginDusk.php
@@ -0,0 +1,10 @@
+<?php
+
+use Laravel\Dusk\Browser;
+
+test('login', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/login')
+            ->assertSee('Login');
+    });
+});
