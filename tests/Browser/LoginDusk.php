da1c9a9e8824c4cfd84e4de6ddf83b1b01e8217e
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2024-03-29 02:16:19 +0530

    Update LoginDusk.php

--- tests/Browser/LoginDusk.php
+++ tests/Browser/LoginDusk.php
@@ -5,6 +5,6 @@ use Laravel\Dusk\Browser;
 test('login', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/login')
-            ->assertSee('Login');
+            ->assertSee('Google');
     });
 });
